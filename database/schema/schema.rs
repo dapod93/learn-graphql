9e892e83cfdb98b5f21feb340f447f85ef7ef116
dapod93 <davidliois50@gmail.com>
2025-06-09 12:31:51 +0700

    update: files
    Signed-off-by: dapod93 <davidliois50@gmail.com>

--- database/schema/schema.rs
+++ database/schema/schema.rs
@@ -1,10 +1,2 @@
 // @generated automatically by Diesel CLI.
 
-diesel::table! {
-    users (id) {
-        id -> Integer,
-        first_name -> Nullable<Text>,
-        last_name -> Nullable<Text>,
-        email -> Nullable<Text>,
-    }
-}
