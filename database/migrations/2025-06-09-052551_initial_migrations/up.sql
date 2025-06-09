b54cb478a15ddb1faefcbb43e1590ac872efc78c
dapod93 <davidliois50@gmail.com>
2025-06-09 12:29:17 +0700

    update: files
    Signed-off-by: dapod93 <davidliois50@gmail.com>

--- database/migrations/2025-06-09-052551_initial_migrations/up.sql
+++ database/migrations/2025-06-09-052551_initial_migrations/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
-CREATE TABLE IF EXISTS users (
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     first_name VARCHAR,
     last_name VARCHAR,
-    email VARCHAR,
+    email VARCHAR
 )
