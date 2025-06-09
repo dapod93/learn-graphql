dffae71fd1eb96494e02571ad6b57a3d29c75e1f
dapod93 <davidliois50@gmail.com>
2025-06-09 15:16:57 +0700

    update: files
    Signed-off-by: dapod93 <davidliois50@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod database;
 
 mod common;
 mod router;
-mod routes;
-mod schema;
 mod user;
+
+use std::sync::Arc;
 
 use actix_cors::Cors;
 use actix_web::{
@@ -15,11 +15,7 @@ use actix_web::{
 use dotenvy::dotenv;
 use juniper::http::{GraphQLRequest, graphiql::graphiql_source};
 
-use crate::{
-    database::connection::establish_connection,
-    routes::ping::rping,
-    schema::ping::{Schema, create_schema},
-};
+use crate::{database::connection::establish_connection, router::graphql::GraphQLSchema};
 
 #[get("/graphiql")]
 async fn graphql_playground() -> impl Responder {
@@ -27,8 +23,11 @@ async fn graphql_playground() -> impl Responder {
 }
 
 #[route("/graphql", method = "GET", method = "POST")]
-async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> impl Responder {
-    let user = data.execute(&st, &()).await;
+async fn graphql(
+    st: web::Data<Arc<GraphQLSchema>>,
+    data: web::Json<GraphQLRequest>,
+) -> impl Responder {
+    let user = data.execute(&st.schema, &st.context).await;
     HttpResponse::Ok().json(user)
 }
 
@@ -37,21 +36,17 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    let db_pool = establish_connection();
-
-    let schema = std::sync::Arc::new(create_schema());
-    let port = 8081;
+    let schema = Arc::new(GraphQLSchema::new(Arc::new(establish_connection())));
 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::from(schema.clone()))
             .service(graphql)
             .service(graphql_playground)
-            .service(rping)
             .wrap(Cors::permissive())
             .wrap(middleware::Logger::default())
     })
-    .bind(("0.0.0.0", port))?
+    .bind(("0.0.0.0", 8081))?
     .run()
     .await
 }
