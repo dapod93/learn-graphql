2f078644e08a232eab7e475f3f19f82d2ff899a9
dapod93 <davidliois50@gmail.com>
2025-06-09 11:52:50 +0700

    update: files
    Signed-off-by: dapod93 <davidliois50@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,46 @@
-mod account;
+mod routes;
+mod schema;
 
-fn main() {
-    println!("Hello, world!");
+use actix_cors::Cors;
+use actix_web::{
+    App, HttpResponse, HttpServer, Responder, get, middleware, route,
+    web::{self, Html},
+};
+use juniper::http::{GraphQLRequest, graphiql::graphiql_source};
+
+use crate::{
+    routes::ping::Rping,
+    schema::ping::{Schema, create_schema},
+};
+
+#[get("/graphiql")]
+async fn graphql_playground() -> impl Responder {
+    Html::new(graphiql_source("/graphql", None))
+}
+
+#[route("/graphql", method = "GET", method = "POST")]
+async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> impl Responder {
+    let user = data.execute(&st, &()).await;
+    HttpResponse::Ok().json(user)
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let schema = std::sync::Arc::new(create_schema());
+    let port = 8081;
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::from(schema.clone()))
+            .service(graphql)
+            .service(graphql_playground)
+            .service(Rping)
+            .wrap(Cors::permissive())
+            .wrap(middleware::Logger::default())
+    })
+    .bind(("0.0.0.0", port))?
+    .run()
+    .await
 }
